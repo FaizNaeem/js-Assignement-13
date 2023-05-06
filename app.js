401b984f3434e516e92e992cee0de4a75b0984a9
FaizNaeem <faiznaeem192022@gmail.com>
2023-05-06 13:59:03 +0500

    first

---
+++ app.js
@@ -0,0 +1,87 @@
+  /////////////// question 1
+        // var date=new Date()
+        // document.write(date);
+
+
+        //////////////question 2
+        // var arry = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        // var date=new Date()
+        // var month=date.getMonth()
+        // var final=arry[month]
+        //  alert("current month: "+ final)
+
+
+        ////////// question 3
+        // var arry=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+        // var date=new Date()
+        // var day=date.getDay()
+        // var done=arry[day]
+        // alert("Today is " + done)
+   
+
+        ///////////////question 4
+        // var arry=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+        // var date=new Date()
+        // var day=date.getDay()
+        // var done=arry[day]
+        // if(done=="sat" ||done=="sun"){
+        //     alert("its Fun day 😍")
+        // }
+        // else{alert("Its booring day😒")}
+
+
+/////////////////////// question 5
+        // var date=new Date()
+        // var day=date.getDate()
+        // if(day<16){
+
+        //     alert("“First fifteen days of the month”")
+        // }
+        // else{
+        //     alert("“last fifteen days of the month”")
+        // }
+   
+
+        /////////////  question 6
+    //    var date=new Date("Jan. 1, 1970")
+    //    var minus=new Date()
+    //    var cal=(date-minus)
+    //    var ab=Math.abs(cal)
+    //    document.write(minus+"<br>")
+    //    document.write("Elasped millisecond since Jan. 1, 1970 " +ab)
+       
+        
+        //////////// question 7
+        // var date=new Date()
+        // var check=date.getHours()
+        // if(check<=12){
+        //     alert("Its AM")
+
+        // }
+        // else{
+        //     alert("Its PM")
+        // }
+
+
+        /////////// question 8
+        //   var laterDate = new Date(2020, 11, 31);
+        //  document.write(laterDate);
+
+        /////////////// question 9//
+        //  var ramadanStart = new Date();
+        // var First = new Date('June 18, 2015');
+        // var days= Math.round((ramadanStart - First) / (1000 * 60 * 60 * 24));
+        // alert(`Number of days past since 1st Ramadan: ${days}`);
+
+        ////////  question 11
+    //     var show=new Date()
+    //    var now=new Date()
+    //    now.setHours(now.getHours()-1)
+    //    document.write(show +"<br>")
+    //    document.write("1 hours ago it was " + now )
+
+
+///////////////////////// question 12
+        // var currentDate = new Date();
+        // currentDate.setFullYear(currentDate.getFullYear() - 100);
+        // alert(`Date 100 years back: ${currentDate}`);
